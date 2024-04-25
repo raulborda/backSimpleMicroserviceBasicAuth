09956f863bc7e29a3efaf2f69903499199b7ae7c
Raul <raul.borda@binamics.com.ar>
2024-04-25 16:11:36 -0300

    cambio de PORT + index.html

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const axios = require('axios');
 const basicAuth = require('basic-auth');
 require('dotenv').config();
+const cors = require('cors');
 
 
 const app = express();
 const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
 
 //Función para authBasic
 const auth = (req, res, next) => {
@@ -24,7 +27,7 @@ app.get('/sum', auth, async (req, res) => {
     const { data: { num1, num2 } } = await axios.get('http://localhost:6001/random');
     const suma = num1 + num2;
     res.json({ suma });
-    
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error al sumar los números' });
